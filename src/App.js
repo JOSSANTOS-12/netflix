4d6a8b046bf329ee817acc0a1134106c7dd4d58e
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-04-08 12:07:55 +0300

    First Commi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
